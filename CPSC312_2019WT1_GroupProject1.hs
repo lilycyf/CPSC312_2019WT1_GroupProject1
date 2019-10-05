89d9377be7c49a898df265e4bde9073b5ee0375c
lilycyf <49101372+lilycyf@users.noreply.github.com>
2019-10-04 20:13:03 -0700

    Update CPSC312_2019WT1_GroupProject1.hs

--- CPSC312_2019WT1_GroupProject1.hs
+++ CPSC312_2019WT1_GroupProject1.hs
@@ -6,27 +6,45 @@
 -- Name: 
 -- Student Number: 
 
-data Sudoku = Sudoku [(int,int)]
-data Sudoku = Sudoku [int]
+-- with the first integer as Value and second integer as position
+data Sudoku = Sudoku [(Int, Int)]
 
-solve :: Sudoku -> IO Sudoku or false
-solve = solveloS
+-- solve :: Sudoku -> IO Sudoku or false
+-- solve = solveloS
 
-solveS S = if solved? S then S else solveloS (nextBoards S)
+-- solveS S = if solved? S then S else solveloS (nextBoards S)
 
-solveloS [] = 0
-solveloS (h:t) = if ((solveS h) /= False) then (solveS h) else solveloS t
+-- solveloS [] = 0
+-- solveloS (h:t) = if ((solveS h) /= False) then (solveS h) else solveloS t
 
-solved?
+-- solved
 
-nextBoards
+-- nextBoards
 
-fillWith1to9
+-- find the place with no value
+findBlanks :: Sudoku -> [Int]
+findBlanks S = filter (notElem (position S)) [1..99]
 
-keepOnlyValid
+-- find the place with value
+position :: Sudoku -> [Int]
+position S = map (\(v,p) -> p) S
 
-ReadSquare
+-- fill position i with 1 to 9
+fillWith1to9 :: Int -> Sudoku -> [Sudoku]
+fillWith1to9 i S = [[(x,i):S] | x<-[1..9]]
 
-FillSquare S p i = ()
+
+-- pick a position to fill with a value and remove all the invalid Sudoku
+keepOnlyValid :: Int -> [Sudoku] -> [Sudoku]
+keepOnlyValid i [] = []
+keepOnlyValid i (h:t) = if (validBoard i h) 
+   then h: keepOnlyValid i t 
+   else keepOnlyValid i t
+
+-- check the cloum, row, and block that the position i is in
+validBoard :: Int -> Sudoku -> Bool
+validBoard i S = 
+
+
 
 
