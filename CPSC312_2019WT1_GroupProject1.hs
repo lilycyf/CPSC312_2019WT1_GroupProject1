e764df804a5ca817b48ec3f57f9f6c9d14a86939
lilycyf <49101372+lilycyf@users.noreply.github.com>
2019-10-05 02:29:25 -0700

    Update CPSC312_2019WT1_GroupProject1.hs
    able to solve testSudokus

--- CPSC312_2019WT1_GroupProject1.hs
+++ CPSC312_2019WT1_GroupProject1.hs
@@ -8,8 +8,6 @@
 
 -- with the first integer as Value and second integer as position
 data Sudoku = Sudoku [(Integer, Integer)]
-data Result = ResultS [(Integer, Integer)]
-            | ResultF Bool
 
 testSudoku = [(2,1),(7,2),(4,3),(8,4),(9,5),(1,6),(3,7),(6,8),(5,9),
               (1,10),(3,11),(8,12),(5,13),(2,14),(6,15),(4,16),(9,17),(7,18),
